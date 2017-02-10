ec2136449c7eec93a2e0c7cdc6d6db91f75564a2
ValentinN <v.nahim@gmail.com>
2017-02-10 18:10:47 +0100

    public

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 /**
- * Class PACKAGE_NAME.Main
- * @author vnahi
- * @project TD7_COO
+ * Created by valentinL on 10/02/2017.
  */
-public class Main { }
+public class Main {
+
+    public static void main(String args[]){
+
+
+        Tauren diablon = new Tauren("Diablon", 100, Bouclier, 15); // fais gaffe a l'attribut arme
+        Orc azag = new Orc("Azag", 100, /*arme*/, /*mettre attribut "final String tailleOreilles" dans Orc pour mettre taille oreilles ici*/);
+
+        diablon.attaquer(azag);
+        azag.attaquer(diablon);
+
+
+
+
+    }
+}
